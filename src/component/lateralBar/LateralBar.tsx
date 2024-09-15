35df5dae6cbb7c4b3d6f26080ccbdbb5f443387f
emili <emilio3020@gmail.com>
2024-09-15 14:45:36 +0200

    ref/lateralBar

--- src/component/lateralBar/LateralBar.tsx
+++ src/component/lateralBar/LateralBar.tsx
@@ -4,79 +4,63 @@ import { ChevronLeft, ChevronRight } from "lucide-react";
 
 import "./LateralBar.css";
 
-function LateralBar({setActiveSection, collapsed, setCollapsed}) {
+const sections = [
+  {
+    src: "https://cdn-icons-png.flaticon.com/512/1063/1063376.png",
+    alt: "Experiencia",
+    section: "experience"
+  },
+  {
+    src: "https://cdn-icons-png.flaticon.com/512/2502/2502399.png",
+    alt: "Habilidades",
+    section: "skills"
+  },
+  {
+    src: "https://cdn-icons-png.flaticon.com/512/5129/5129551.png",
+    alt: "Joke",
+    section: "joke"
+  },
+  {
+    src: "https://cdn-icons-png.flaticon.com/512/2740/2740648.png",
+    alt: "Otros",
+    section: "other"
+  }
+]
 
-    return (
-      <Sidebar collapsed={collapsed} className="h-screen">
-          <Menu>
-            <MenuItem
-              icon={
-                collapsed ? (
-                  <ChevronRight size={20} />
-                ) : (
-                  <ChevronLeft size={20} />
-                )
-              }
-              onClick={() => setCollapsed(!collapsed)}
-              className="menu-item"
-            >
-              {!collapsed && <h2>Menú</h2>}
-            </MenuItem>
-            <MenuItem
-              icon={
-                <img
-                  src="https://cdn-icons-png.flaticon.com/512/1063/1063376.png"
-                  alt="Experiencia"
-                  className="rounded-full"
-                  width={25}
-                />
-              }
-              onClick={() => setActiveSection("experience")}
-            >
-              Experiencia
-            </MenuItem>
-            <MenuItem
-              icon={
-                <img
-                  src="https://cdn-icons-png.flaticon.com/512/2502/2502399.png"
-                  alt="Habilidades"
-                  className="rounded-full"
-                  width={25}
-                />
-              }
-              onClick={() => setActiveSection("skills")}
-            >
-              Habilidades
-            </MenuItem>
-            <MenuItem
-              icon={
-                <img
-                  src="https://cdn-icons-png.flaticon.com/512/5129/5129551.png"
-                  alt="Joke"
-                  className="rounded-full"
-                  width={25}
-                />
-              }
-              onClick={() => setActiveSection("joke")}
-            >
-              Joke
-            </MenuItem>
-            <MenuItem
-              icon={
-                <img
-                  src="https://cdn-icons-png.flaticon.com/512/2740/2740648.png"
-                  alt="Otros"
-                  className="rounded-full"
-                  width={25}
-                />
-              }
-              onClick={() => setActiveSection("other")}
-            >
-              Otros Temas
-            </MenuItem>
-          </Menu>
-        </Sidebar>
-    );
+function LateralBar({ setActiveSection, collapsed, setCollapsed }) {
+
+  return (
+    <Sidebar collapsed={collapsed} className="h-screen">
+      <Menu>
+        <MenuItem
+          icon={
+            collapsed ? (
+              <ChevronRight size={20} />
+            ) : (
+              <ChevronLeft size={20} />
+            )
+          }
+          onClick={() => setCollapsed(!collapsed)}
+          className="menu-item"
+        >
+          {!collapsed && <h2>Menú</h2>}
+        </MenuItem>
+        {sections.map(section=> <MenuItem
+          icon={
+            <img
+              src={section.src}
+              alt={section.alt}
+              className="rounded-full"
+              width={25}
+            />
+          }
+          onClick={() => setActiveSection(section.section)}
+        >
+          {section.alt}
+        </MenuItem>)}
+      </Menu>
+    </Sidebar>
+  );
 }
 
 export default LateralBar;
